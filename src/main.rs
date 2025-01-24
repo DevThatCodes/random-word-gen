5b7504bd4a435d4240362fa6bbca3c4158765e2b
DevThatCodes <cupcooklinux@gmail.com>
2025-01-24 17:27:23 -0500

    added higher likeliness for the word to start with a consonant

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ enum LetterTypes {
     None
 }
 
-fn generate_word(wordlen: i32, max_vowels_in_a_row: i32, max_consonants_in_a_row: i32, consonant_mutation_chance: i32, vowel_mutation_chance: i32) -> (String, String) {
+fn generate_word(wordlen: i32, max_vowels_in_a_row: i32, max_consonants_in_a_row: i32, consonant_mutation_chance: i32, vowel_mutation_chance: i32) -> String {
     // variables related to word making
-    let consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
-    let vowels = ["a","e","i","o","u"];
+    let consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z", "r", "s", "t", "l", "n"];
+    let vowels = ["a","e","i","o","u", "e"];
     let mut word = String::new();
     let mut i = 0;
     let mut last_letter_type : LetterTypes = LetterTypes::None;
@@ -30,17 +30,16 @@ fn generate_word(wordlen: i32, max_vowels_in_a_row: i32, max_consonants_in_a_row
 
         // starting letter
         if last_letter_type == LetterTypes::None {
-            if rand(2) == 1 {
-                word += "C";
-                streak += 1;
-                last_letter_type = LetterTypes::Consonant;
-                continue;
-            } else {
+            if rand(5) == 1 {
                 word += "V";
                 streak += 1;
                 last_letter_type = LetterTypes::Vowel;
-                continue;
+            } else {
+                word += "C";
+                streak += 1;
+                last_letter_type = LetterTypes::Consonant;
             }
+            continue;
         }
 
         if last_letter_type == LetterTypes::Vowel {
@@ -56,13 +55,11 @@ fn generate_word(wordlen: i32, max_vowels_in_a_row: i32, max_consonants_in_a_row
                 streak = 0;
                 word += "C";
                 last_letter_type = LetterTypes::Consonant;
-                continue;
             } else {
                 streak += 1;
                 word += "V";
-                continue;
             }
-
+            continue;
         }
 
         if last_letter_type == LetterTypes::Consonant {
@@ -78,12 +75,11 @@ fn generate_word(wordlen: i32, max_vowels_in_a_row: i32, max_consonants_in_a_row
                 streak = 0;
                 word += "V";
                 last_letter_type = LetterTypes::Vowel;
-                continue;
             } else {
                 streak += 1;
                 word += "C";
-                continue;
             }
+            continue;
         }
     }
 
@@ -91,14 +87,14 @@ fn generate_word(wordlen: i32, max_vowels_in_a_row: i32, max_consonants_in_a_row
     // replacing C with a consonant and V with a vowel 
     
     let mut result_word = String::new();
-    for char in word.clone().chars() {
+    for char in word.chars() {
         if char == 'C' {
             result_word += consonants[(rand(consonants.len().try_into().unwrap())-1) as usize];
         } else if char == 'V' {
             result_word += vowels[(rand(vowels.len().try_into().unwrap())-1) as usize];
         }
     }
-    (result_word, word)
+    result_word
 }
 
 fn main() {
@@ -119,25 +115,26 @@ fn main() {
     // word settings
     let max_vowels_in_a_row = 2;
     let max_consonants_in_a_row = 3;
-    let consonant_mutation_chance = 3; // chance for something to become a consonant ( 1 in ? )
-    let vowel_mutation_chance = 2; // chance for something to become a vowel ( 1 in ? )
+    let consonant_mutation_chance = 6; // chance for something to become a consonant ( 1 in ? )
+    let vowel_mutation_chance = 5; // chance for something to become a vowel ( 1 in ? )
     
     let mut result = String::new();
 
     if !target_word.is_empty() {
         // start timer
-        let stopwatch = Instant::now();
         let mut tries = 0;
+        let stopwatch = Instant::now();
         while result != target_word {
-            result = generate_word(wordlen, max_vowels_in_a_row, max_consonants_in_a_row, consonant_mutation_chance, vowel_mutation_chance).0;
+            result = generate_word(wordlen, max_vowels_in_a_row, max_consonants_in_a_row, consonant_mutation_chance, vowel_mutation_chance);
             print!("{} | ", result);
             tries += 1;
         }
         let time_finished = stopwatch.elapsed();
         println!("\n\n Got target word \'{target_word}\' as \'{result}\' in {tries} tries, which took {:.2?}!\n", time_finished);
     } else {
-        result = generate_word(wordlen, max_vowels_in_a_row, max_consonants_in_a_row, consonant_mutation_chance, vowel_mutation_chance).0; 
+        result = generate_word(wordlen, max_vowels_in_a_row, max_consonants_in_a_row, consonant_mutation_chance, vowel_mutation_chance); 
     }
 
     println!("{result} {wordlen}");
 }
+
